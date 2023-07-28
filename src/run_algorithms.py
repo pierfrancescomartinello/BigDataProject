8b4d82aeb870ebd6b3abe28b9c249de18244277b
Francesca Palazzotto <94184791+francescapalazzotto@users.noreply.github.com>
2023-07-28 12:17:42 +0200

    Aggiunta funzione par. ALS test + run_algorithms 	modified:   src/run_algorithms.py 	modified:   src/test.ipynb

--- src/run_algorithms.py
+++ src/run_algorithms.py
@@ -4,9 +4,10 @@ from pyspark.ml.linalg import Vectors
 from pyspark.ml.recommendation import ALS
 from pyspark.sql import SparkSession
 import pandas as pd
-
+import random
 from poll_processing import execute_pipeline, directories, init_spark
-
+import os 
+os.environ["PYARROW_IGNORE_TIMEZONE"] = "1"
 
 def run_collaborative_filtering(
     spark: SparkSession,
@@ -97,7 +98,64 @@ def run_kmeans(spark: SparkSession, df_indexes) -> None:
 
     summary.cluster.show(52)
 
+def tune_ALS(train_data, validation_data, maxIter, regParams, ranks):
+    """
+    grid search function to select the best model based on RMSE of
+    validation data
+    Parameters
+    ----------
+    train_data: spark DF with columns ['userId', 'movieId', 'rating']
+    
+    validation_data: spark DF with columns ['userId', 'movieId', 'rating']
+    
+    maxIter: int, max number of learning iterations
+    
+    regParams: list of float, one dimension of hyper-param tuning grid
+    
+    ranks: list of float, one dimension of hyper-param tuning grid
+    
+    Return
+    ------
+    The best fitted ALS model with lowest RMSE score on validation data
+    """
+    # initial
+    min_error = float('inf')
+    best_rank = -1
+    best_regularization = 0
+    best_model = None
+    for rank in ranks:
+        for reg in regParams:
+            # get ALS model
+            als = ALS().setMaxIter(maxIter).setRank(rank).setRegParam(reg)
+            # train ALS model
+            model = als.fit(train_data)
+            # evaluate the model by computing the RMSE on the validation data
+            predictions = model.transform(validation_data)
+            evaluator = RegressionEvaluator(metricName="rmse",
+                                            labelCol="rating",
+                                            predictionCol="prediction")
+            rmse = evaluator.evaluate(predictions)
+            print('{} latent factors and regularization = {}: '
+                  'validation RMSE is {}'.format(rank, reg, rmse))
+            if rmse < min_error:
+                min_error = rmse
+                best_rank = rank
+                best_regularization = reg
+                best_model = model
+    print('\nThe best model has {} latent factors and '
+          'regularization = {}'.format(best_rank, best_regularization))
+    return best_model
 
 if __name__ == "__main__":
     spark = init_spark()
     df = execute_pipeline(spark, directories)
+
+    ratings = df
+    (training, test) = ratings.randomSplit([0.8,0.2])
+
+    ranks = [1.5, 3, 5, 10.5, 15, 20, 20.5, 30]
+    regParams = [0.01, 0.1, 0.05, 0.7, 1.0, 1.5]
+    M = tune_ALS(training, test, 10, regParams, ranks)
+    print("********STO STAMPANDO********")
+    print("model=",M)
+
